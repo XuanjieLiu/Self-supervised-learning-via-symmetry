948d9b1a292136ac20312b8d32eafe12e1033013
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2022-08-26 07:16:55 +0800

    path fix

--- codes/S3Ball/model_gru_no_rnn/main_train.py
+++ codes/S3Ball/model_gru_no_rnn/main_train.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append('{}{}'.format(os.path.dirname(os.path.abspath(__file__)), '/../../../'))
+
 from train_config import CONFIG
 from trainer_symmetry import BallTrainer, is_need_train
 
