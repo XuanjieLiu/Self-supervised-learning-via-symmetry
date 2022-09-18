a9a64a55dfc251788c27209a1690089ad068c024
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2022-09-18 22:04:54 +0800

    11

--- codes/S3Ball/model_denseCP/eval_z.py
+++ codes/S3Ball/model_denseCP/eval_z.py
@@ -13,7 +13,7 @@ IMG_PATH = 'vae3DBallEval_ImgBuffer'
 IGM_NAME = IMG_PATH + "/test.png"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-MODEL_PATH = 'checkpoint_10000.pt'
+MODEL_PATH = 'checkpoint_30000.pt'
 
 
 def init_img_path():
