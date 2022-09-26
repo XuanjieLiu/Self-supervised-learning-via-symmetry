47eb5daa7371f6cf168bafa28a8261824199f7b2
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2022-09-26 21:10:51 +0800

    Update batch_exp_FindExpGroup.py

--- codes/S3Ball/batch_exp_FindExpGroup.py
+++ codes/S3Ball/batch_exp_FindExpGroup.py
@@ -1,5 +1,6 @@
 import os
-
+import sys
+sys.path.append('{}{}'.format(os.path.dirname(os.path.abspath(__file__)), '/../../'))
 from codes.S3Ball.evalLinearity_A_with_four_zeros_encode import data2z
 from evalLinearity_A_with_four_zeros_shared import ExpGroup
 from typing import List
