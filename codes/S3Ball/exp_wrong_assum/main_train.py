fbe90f9c05eb86bb6f89e5c83b59f808de8603db
Daniel Chin <daniel.chin@nyu.edu>
2022-11-06 18:04:00 +0400

    static anal hack

--- codes/S3Ball/exp_wrong_assum/main_train.py
+++ codes/S3Ball/exp_wrong_assum/main_train.py
@@ -12,13 +12,17 @@ except ImportError:
     sys.path.append(temp_dir)
     from trainer_symmetry import BallTrainer, is_need_train
     assert sys.path.pop(-1) == temp_dir
+else:
+    raise RuntimeError('error egmw5n4i3p7regt')
 
 try:
-    # this branch for static analysis
-    from ..model.train_config import CONFIG
-except ImportError:
     # this branch for runtime
     from train_config import CONFIG
+except ImportError:
+    # this branch for static analysis
+    from ..model.train_config import CONFIG
+    if 1 == 1:
+        raise RuntimeError('error o487wryhalegt')
 
 trainer = BallTrainer(CONFIG)
 if is_need_train(CONFIG):
